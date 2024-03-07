07753cf35f45fe36f2d74200dd44f9d4da321e65
bradlsum <sumnerjbradley@gmail.com>
2024-03-06 20:33:11 -0700

    Working

--- go_image_upscale.go
+++ go_image_upscale.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"image"
+	"image/color"
 	"image/jpeg"
+	"image/png"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func openImage(path string) (image.Image, error) {
@@ -29,27 +32,93 @@ func (i httpImage) serveImage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func resize(img image.Image, scale int) image.Image {
+func integerScale(img image.Image, scale int) image.Image {
 	x_max, y_max := img.Bounds().Max.X*scale, img.Bounds().Max.Y*scale
-	new_image := image.NewNRGBA(image.Rect(0, 0, x_max, y_max))
+	new_img := image.NewNRGBA(image.Rect(0, 0, x_max, y_max))
 	for x := 0; x < x_max; x++ {
 		for y := 0; y < y_max; y++ {
 			c := img.At(x/scale, y/scale)
-			new_image.Set(x, y, c)
+			new_img.Set(x, y, c)
 		}
 	}
-	return new_image
+	return new_img
+}
+
+type Coords struct {
+	x int
+	y int
+}
+
+func (c Coords) toString() string {
+	return strconv.Itoa(c.x) + "," + strconv.Itoa(c.y)
+}
+
+func image2X(img image.Image) image.Image {
+	scale := 2
+	x_max, y_max := img.Bounds().Max.X*scale, img.Bounds().Max.Y*scale
+	new_img := image.NewNRGBA(image.Rect(0, 0, x_max, y_max))
+	visited := make(map[string]bool)
+	for x := 0; x < img.Bounds().Max.X; x++ {
+		for y := 0; y < y_max; y++ {
+			c := img.At(x, y)
+			new_img.Set(x*scale, y*scale, c)
+			coord := Coords{x: x * scale, y: y * scale}
+			visited[coord.toString()] = true
+		}
+	}
+	for x := 0; x < x_max; x++ {
+		for y := 0; y < y_max; y++ {
+			coord := Coords{x, y}
+			c := findNearestColor(new_img, coord, scale, visited)
+			new_img.Set(x, y, c)
+		}
+	}
+	return new_img
+}
+
+func findNearestColor(img image.Image, coord Coords, scale int, visited map[string]bool) color.Color {
+	sample := make(map[color.Color]int)
+	ratio := scale / 2
+	for i := coord.x - ratio; i < coord.x+ratio; i++ {
+		for j := coord.y - ratio; j < coord.y+ratio; j++ {
+			coord := Coords{i, j}
+			v, ok := visited[coord.toString()]
+			if ok && v {
+				c := img.At(i, j)
+				_, ok := sample[c]
+				if ok {
+					sample[c]++
+				} else {
+					sample[c] = 1
+				}
+			}
+		}
+	}
+	var choice color.Color
+	count := 0
+	for i, v := range sample {
+		if count < v {
+			choice = i
+			count = v
+		}
+	}
+	return choice
 }
 
 func main() {
-	img, err := openImage("./Go_gopher.jpg")
-	if err != nil {
-		panic(err)
+	// img, err := openImage("./Go_gopher.jpg")
+	// img, err := openImage("./gopher1.jpg")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	r, _ := os.Open("./lil_gopher.png")
+	img, _ := png.Decode(r)
+
+	for i := 0; i < 10; i++ {
+		img = image2X(img)
 	}
 
-	new_img := resize(img, 4)
-
-	http_image := httpImage{new_img}
+	http_image := httpImage{img}
 	http.HandleFunc("/", http_image.serveImage)
 	http.ListenAndServe(":8080", nil)
 }
